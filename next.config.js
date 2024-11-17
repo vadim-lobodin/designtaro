7582423d1323a533745f2f04fcc66185b61c7673
Vadim.Lobodin <vadim.lobodin@jetbrains.com>
2024-11-17 01:03:48 +0100

    updates

--- next.config.js
+++ next.config.js
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  experimental: {
-    outputFileTracingRoot: process.cwd(),
-  },
   images: {
     unoptimized: true,
   },
